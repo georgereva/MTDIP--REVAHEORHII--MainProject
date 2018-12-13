69c54a4419862bb3ebac4b558af7036f997e5b11
georgereva <joravartan@gmail.com>
2018-12-13 22:43:02 +0200

    StackDemoClass

---
+++ src/StackDemo.java
@@ -0,0 +1,37 @@
+package First_Prog.Glava_18;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+class StackDeo {
+    static void showpush(Stack<Integer> st, int a) {
+        st.push(a);
+        System.out.println(a);
+        System.out.println(st);
+    }
+
+    static void showpop(Stack<Integer> st) {
+        Integer a = st.pop();
+        System.out.println(a);
+        System.out.println(st);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        System.out.println(st);
+
+        showpush(st, 42);
+        showpush(st, 66);
+        showpush(st, 99);
+        showpop(st);
+        showpop(st);
+        showpop(st);
+
+        try {
+            showpop(st);
+        } catch (EmptyStackException e) {
+            System.out.println("Stack is empty.");
+        }
+    }
+}
